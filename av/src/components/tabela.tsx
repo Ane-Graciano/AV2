b0fc2788a7a3db332d9fe7e54541f4ec136fe562
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-01 13:47:38 -0300

    feat: cria o componente de tabela

---
+++ av/src/components/tabela.tsx
@@ -0,0 +1,47 @@
+import { Component } from "react"
+
+export type Coluna = {
+    header: string
+    accessor: string
+}
+
+interface PropsTabela {
+    dados: any[]
+    colunas: Coluna[]
+}
+
+interface StateTabela { }
+
+export default class Tabela extends Component<PropsTabela, StateTabela> {
+    render() {
+        return (
+            <>
+                <table className="w-full overflow-hidden rounded-lg border-collapse">
+                    <thead className="bg-[#3a6ea5] text-white">
+                        <tr>
+                            {this.props.colunas.map((col, colIndex) => (
+                                <th key={colIndex} className="p-3 text-center">
+                                    {col.header}
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.dados.map((d,dIndex) => (
+                            <tr key={dIndex} className={`${dIndex % 2 === 0 ? 'bg-[#f9f9f9]' : 'bg-[#e3f2fd]'}`}>
+                                {this.props.colunas.map((col) => {
+                                    const valor = d[col.accessor]
+                                    return(
+                                        <td key={col.header} className="p-3 border-b-2 border-white">
+                                            {valor}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </>
+        )
+    }
+}
