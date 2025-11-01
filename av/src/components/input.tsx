3a195100419bf86007566fcfd7308ac47326f06b
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-01 13:47:55 -0300

    feat: cria o componente de input

---
+++ av/src/components/input.tsx
@@ -0,0 +1,42 @@
+import type React from "react"
+import { Component } from "react"
+
+type PropsInput = {
+    id?: string,
+    htmlfor?: string,
+    children?: React.ReactNode,
+    type: string,
+    name: string,
+    value?: string,
+    palceholder?: string,
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    classNameInput?: string
+    classNameLabel?: string
+}
+
+export default class InputLinha extends Component<PropsInput,{}>{
+    constructor(props:PropsInput){
+        super(props)
+    }
+    render(){
+        return(
+            <section className="relative">
+                <input 
+                    type={this.props.type}
+                    id={this.props.id}
+                    name={this.props.name}
+                    placeholder={this.props.palceholder}
+                    value={this.props.value}
+                    onChange={this.props.onChange}
+                    className={`w-[300px] block px-2 pb-1 pt-5 text-sm text-gray-950 border-b-2 border-[#c0c0c0]  focus:outline-none focus:border-[#3a6ea5] peer  ${this.props.classNameInput}`}
+                />
+                <label 
+                    htmlFor={this.props.htmlfor}
+                    className={`absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 start-2 peer-focus:text-[#3a6ea5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto ${this.props.classNameLabel}`}
+                >
+                    {this.props.children}
+                </label>
+            </section>
+        )
+    }
+}
