5b9b62314beee3297b0cbe8de58ac74b888ff88a
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-01 13:48:13 -0300

    feat: cria o componente de barra de pesquisa

---
+++ av/src/components/barraPesquisa.tsx
@@ -0,0 +1,36 @@
+import { Component, type ChangeEvent } from "react"
+import InputLinha from "./input";
+
+type PropsBarraPesquisa = {
+    onPesquisa: (texto: string) => void
+    placeholder?: string
+}
+
+interface StateBarraPesquisa {
+    pesquisa: string;
+}
+
+export default class BarraPesquisa extends Component<PropsBarraPesquisa,StateBarraPesquisa>{
+    constructor(props: PropsBarraPesquisa){
+        super(props)
+        this.state = {
+            pesquisa: ''
+        }
+        this.attPesquisa = this.attPesquisa.bind(this)
+    }
+
+    attPesquisa(e: ChangeEvent<HTMLInputElement>){
+        const texto = e.target.value
+        this.setState({pesquisa: texto}, () => {
+            this.props.onPesquisa(texto)
+        })
+    }
+
+    render(){
+        return(
+            <>
+            <InputLinha type="text" name="barraPesquisa" palceholder={this.props.placeholder} value={this.state.pesquisa} onChange={this.attPesquisa}/>
+            </>
+        )
+    }
+}
