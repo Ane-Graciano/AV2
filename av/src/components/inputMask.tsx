db87d0b8639c43b539f0ae241bed91b70eb53c47
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-02 09:06:26 -0300

    feat: criação do componente de input para receber máscara
    criação de um camponente de input flutuante, com a opção de colocar mascara, instalação da biblioteca react-imask

---
+++ av/src/components/inputMask.tsx
@@ -0,0 +1,49 @@
+import { Component } from "react";
+import { IMaskInput } from 'react-imask';
+
+interface PropsInputMask {
+    label: string
+    name: string
+    htmlor?: string
+    id?: string
+    mask: string
+    placeholder?: string
+    required?: boolean
+    maxLength?: number
+    // onAccept: (value: string) => void
+    classNameLabel?: string
+    classNameInput?: string
+}
+
+interface StateInputMask { }
+
+export default class InputMask extends Component<PropsInputMask, StateInputMask> {
+    constructor(props: PropsInputMask) {
+        super(props)
+    }
+    render() {
+
+        return (
+            <>
+                <section className="relative">
+                    <label
+                        htmlFor={this.props.htmlor}
+                        className={`absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 start-2 peer-focus:text-[#3a6ea5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto ${this.props.classNameLabel}`}
+                    >
+                        {this.props.label}
+                    </label>
+                    <IMaskInput
+                        name={this.props.name}
+                        id={this.props.htmlor}
+                        mask={this.props.mask}
+                        placeholder={this.props.placeholder}
+                        required={this.props.required}
+                        maxLength={this.props.maxLength}
+                        // onAccept={(value: any) => this.props.onAccept(value)}
+                        className={`w-[300px] block px-2 pb-1 pt-5 text-sm text-gray-950 border-b-2 border-[#c0c0c0]  focus:outline-none focus:border-[#3a6ea5] peer  ${this.props.classNameInput}`}
+                    />
+                </section>
+            </>
+        )
+    }
+}
