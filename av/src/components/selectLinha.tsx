963896996096073d601dec9c789192d11c5c7419
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-02 22:04:09 -0300

    style: ajuste no estilo

--- av/src/components/selectLinha.tsx
+++ av/src/components/selectLinha.tsx
@@ -7,11 +7,13 @@ type Op = {
 interface PropsSelect{
     name: string
     label: string
+    value?: string
     opcoes : Op[]
     onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void
     required?: boolean
     classNameSelect?: string
     classNameLabel?: string
+    disabled?: boolean
 }
 
 interface StateSelect{}
@@ -29,7 +31,7 @@ export default class SelectLinha extends Component<PropsSelect,StateSelect>{
                     <label htmlFor={this.props.name} className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 start-2 peer-focus:text-[#3a6ea5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
                         {this.props.label}
                     </label>
-                    <select name={this.props.name} id={this.props.name} onChange={this.props.onChange} required={this.props.required} className={`w-[300px] block px-2 pb-1 pt-5 text-sm text-gray-950 border-b-2 border-[#c0c0c0]  focus:outline-none focus:border-[#3a6ea5] peer  ${this.props.classNameSelect}`}>
+                    <select name={this.props.name} value={this.props.value} id={this.props.name} onChange={this.props.onChange} required={this.props.required} disabled={this.props.disabled} className={`w-[300px] block px-2 pb-1 pt-5 text-sm text-gray-950 border-b-2 border-[#c0c0c0]  focus:outline-none focus:border-[#3a6ea5] peer  ${this.props.classNameSelect}`}>
                         {this.props.opcoes.map(op=> (
                             <option key={op.value} value={op.value}>
                                 {op.label}
