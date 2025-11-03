da75938b938bc408891090517c3680ebce82e2be
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-02 22:03:53 -0300

    feat: inserção de metodos para abrir as telas de cadastro
    uso do modal, inseri metodos para abri os cadastros e o modal

--- av/src/components/navbar.tsx
+++ av/src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { TiHomeOutline } from "react-icons/ti";
 import { FaPlaneDeparture } from "react-icons/fa6";
@@ -7,67 +7,133 @@ import { BsPersonAdd } from "react-icons/bs";
 import { BsListCheck } from "react-icons/bs";
 import { FaClipboardList } from "react-icons/fa";
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import Modal from "./modal";
+import CadFunc from "../pages/cadFunc";
+import CadEtapa from "../pages/cadEtapa";
+import CadPeca from "../pages/cadPeca";
+import CadAeronave from "../pages/cadAeronave";
+import CadTeste from "../pages/cadTeste";
 
-
-interface NavBarState {
-    navAberta: boolean;
-}
 
 interface NavBarProps {
 }
 
-export default class NavBar extends Component<NavBarProps, NavBarState> {
-    constructor(props:NavBarState){
-            super(props)
-            this.state = {
-                navAberta: false
-            }
-            this.attNav = this.attNav.bind(this)
-        }
+interface NavBarState {
+    navAberta: boolean
+    modalAberto: boolean
+    conteudoModal: React.ReactNode
+}
 
-    attNav(){
+
+export default class NavBar extends Component<NavBarProps, NavBarState> {
+    constructor(props: NavBarProps) {
+        super(props)
+        this.state = {
+            navAberta: false,
+            modalAberto: false,
+            conteudoModal: null
+        }
+        this.attNav = this.attNav.bind(this)
+        this.abreCadFunc = this.abreCadFunc.bind(this)
+        this.abreCadEtapa = this.abreCadEtapa.bind(this)
+        this.abreCadPeca = this.abreCadPeca.bind(this)
+        this.abreCadAeronave = this.abreCadAeronave.bind(this)
+        this.abreCadTeste = this.abreCadTeste.bind(this)
+    }
+
+    attNav() {
         this.setState(navAnterior => ({
             navAberta: !navAnterior.navAberta
         }))
     }
+
+    abreCadFunc(e: React.MouseEvent) {
+        e.preventDefault()
+        this.setState({
+            conteudoModal: <CadFunc />,
+            modalAberto: true
+        })
+    }
+
+    abreCadEtapa(e: React.MouseEvent){
+        e.preventDefault()
+        this.setState({
+            conteudoModal: <CadEtapa/>,
+            modalAberto: true
+        })
+    }
+
+    abreCadPeca(e: React.MouseEvent){
+        e.preventDefault()
+        this.setState({
+            conteudoModal: <CadPeca/>,
+            modalAberto: true
+        })
+    }
+
+    abreCadAeronave(e: React.MouseEvent){
+        e.preventDefault()
+        this.setState({
+            conteudoModal: <CadAeronave/>,
+            modalAberto: true
+        })
+    }
+
+    abreCadTeste(e: React.MouseEvent){
+        e.preventDefault()
+        this.setState({
+            conteudoModal: <CadTeste/>,
+            modalAberto: true
+        })
+    }
+
     render() {
-        const {navAberta} = this.state
+        const { navAberta, modalAberto, conteudoModal } = this.state
 
         const IconeNav = navAberta ? FaChevronLeft : FaChevronRight
         return (
             <>
                 <nav className={`bg-[#e2eafc] flex flex-col items-center min-h-screen text-white py-6 gap-6 fixed top-0 z-10
-                    ${navAberta ? 'w-[100px]' : 'w-[60px]'}`}>
-                   <button onClick={this.attNav}
-                   className="right-[-15px] top-6 p-1 bg-white rounded-full shadow">
-                        <IconeNav size={10} className="text-[#135b78]"/>
-                   </button>
+                    ${navAberta ? 'w-[15%]' : 'w-[5%]'}`}>
+                    <button onClick={this.attNav}
+                        className="absolute right-[-15px] top-6 p-2 bg-white rounded-full shadow">
+                        <IconeNav size={10} className="text-[#135b78]" />
+                    </button>
                     <section className="flex flex-col space-y-4">
-                        <NavLink to="/home">
-                            <TiHomeOutline size={30} className="text-[#135b78]"/>
+                        <NavLink to="/home" className={`flex`}>
+                            <TiHomeOutline size={30} className="text-[#135b78]" />
+                            {navAberta ? <p>Home</p> : null}
                         </NavLink>
 
-                        <NavLink to="/home">
-                            <FaPlaneDeparture size={30}  className="text-[#135b78]"/>
+                        <NavLink to="#" className={`flex`} onClick={this.abreCadAeronave}>
+                            <FaPlaneDeparture size={30} className="text-[#135b78]"/>
+                            {navAberta ? <p>Cadastra Aeronave</p> : null}
                         </NavLink>
 
-                        <NavLink to="/home">
-                            <IoAirplaneOutline size={30}  className="text-[#135b78]"/>
+                        <NavLink to="/#" className={`flex`} onClick={this.abreCadPeca}>
+                            <IoAirplaneOutline size={30} className="text-[#135b78]" />
+                            {navAberta ? <p>Cadastra Peça</p> : null}
                         </NavLink>
 
-                        <NavLink to="/home">
-                            <BsPersonAdd size={30}  className="text-[#135b78]"/>
+                        <NavLink to="/#" className={`flex`} onClick={this.abreCadFunc}>
+                            <BsPersonAdd size={30} className="text-[#135b78]"/>
+                            {navAberta ? <p>Cadastrar Funcionário</p> : null}
                         </NavLink>
 
-                        <NavLink to="/home">
-                            <BsListCheck size={30}  className="text-[#135b78]"/>
+                        <NavLink to="/home" className={`flex`} onClick={this.abreCadTeste}>
+                            <BsListCheck size={30} className="text-[#135b78]" />
+                            {navAberta ? <p>Registra Teste</p> : null}
                         </NavLink>
 
-                        <NavLink to="/home">
-                            <FaClipboardList size={30}  className="text-[#135b78]"/>
+                        <NavLink to="/#" className={`flex`} onClick={this.abreCadEtapa}>
+                            <FaClipboardList size={30} className="text-[#135b78]"/>
+                            {navAberta ? <p>Cadastra Etapa</p> : null}
                         </NavLink>
                     </section>
                 </nav>
+                <Modal aberto={modalAberto} onFechar={() => this.setState({ modalAberto: false, conteudoModal: null })}>
+                    {conteudoModal}
+                </Modal>
             </>
         )
     }
