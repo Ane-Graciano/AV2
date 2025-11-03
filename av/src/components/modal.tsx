f62089f97de1508230b9d6999a31afb0328e3559
aegsjc@gmail.com <aegsjc@gmail.com>
2025-11-02 22:02:54 -0300

    style: ajuste do estilo do componente
    mudança na estilização

--- av/src/components/modal.tsx
+++ av/src/components/modal.tsx
@@ -6,39 +6,40 @@ type PropsModal = {
     aberto: boolean
     onFechar: () => void
     children: React.ReactNode
-    modalClassName: string
+    modalClassName?: string
 }
 
-export default class Modal extends Component<PropsModal, {}>{
-    constructor(props:PropsModal){
+export default class Modal extends Component<PropsModal, {}> {
+    constructor(props: PropsModal) {
         super(props)
     }
 
     private clicouFora = (e: React.MouseEvent) => {
         if (e.target === e.currentTarget) {
-            this.props.onFechar
+            console.log('clicou fora')
+            this.props.onFechar()
         }
     }
-    
-    render(){
-        const {aberto, children, modalClassName} = this.props
 
-        if(!aberto) return null
+
+    render() {
+        const { aberto, children, modalClassName } = this.props
+        if (!aberto) return null
 
         const modalRoot = document.getElementById('modal-root')
-        if(!modalRoot) return null
+        if (!modalRoot) return null
 
-        return(
-            <>
+        return createPortal(
             <section
-                className={`bg-blue-500 ${modalClassName}`}
+                className={`fixed inset-0 flex items-center justify-center overflow-y-auto bg-black/20 ${modalClassName}`}
                 onClick={this.clicouFora}
                 role="dialog"
                 aria-modal="true">
+                <div className="bg-white p-6 rounded-xl shadow-2xl max-w-[60%] w-full h-[80%] border-2 border-gray-100" onClick={(e) => e.stopPropagation()}>
                     {children}
+                </div>
             </section>,
-            modalRoot
-            </>
+            modalRoot!
         )
     }
 }
